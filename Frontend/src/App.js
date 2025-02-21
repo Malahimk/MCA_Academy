850daaca49bea3cf08a102dae45c8b0cf3769e29
Malahimk <114082346+Malahimk@users.noreply.github.com>
2025-02-21 10:59:10 +0400

    its back

---
+++ Frontend/src/App.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import './App.css';
+import Home from './Screens/Home';
+import About from './Screens/About';
+import HRAcademy from './Screens/HRAcademy';
+import SupplyChainAcademy from './Screens/SupplyChainAcademy';
+import CoachingandMentoring from './Screens/CoachingandMentoring';
+import Finance from './Screens/Finance';
+import Skills from './Screens/Skills';
+import UnlockingAuthenticLeadership from './Screens/UnlockingAuthenticLeadership';
+import EmpoweringWomenLeadersForSuccess from './Screens/EmpoweringWomenLeadersForSuccess';
+import Fostering from './Screens/Fostering';
+import Coaching from './Screens/Coaching';
+import ContactUs from './Screens/ContactUs';
+import NewScreen from './Screens/NewScreen';
+import NewScreen2 from './Screens/NewScreen2';
+import NewScreen3 from './Screens/NewScreen3';
+import NewScreen4 from './Screens/NewScreen4';
+import NewScreen5 from './Screens/NewScreen5';
+import xyz from './Screens/xyz';
+
+// import FinancialReporting from './Screens/FinancialReporting';
+// import GrowthProgram from './Screens/GrowthProgram';
+// import Budgeting from './Screens/Budgeting';
+// import Modeling from './Screens/Modeling';
+// import WorkingCapital from './Screens/WorkingCapital';
+// import DecisionMaking from './Screens/DecisionMaking';
+// import Credit from './Screens/Credit';
+
+import Unlocking from './Screens/Unlocking';
+import Harmony from './Screens/Harmony';
+
+import Stratergy from './Screens/Stratergy';
+import Global from './Screens/Global';
+import Leading from './Screens/Leading';
+
+import Leadership from './Screens/Leadership';
+import Emerging from './Screens/Emerging';
+import Development from './Screens/Development';
+import Executive from './Screens/Executive';
+
+// import HRBusiness from './Screens/HRBusiness';
+// import HRAnalytics from './Screens/HRAnalytics';
+// import YoungHR from './Screens/YoungHR';
+
+
+function App() {
+  useEffect(() => {
+    document.body.style.zoom = '90%';
+  }, []);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/HRProgram' element={<HRAcademy />} />
+        <Route exact path='/FinanceProgram' element={<Finance />} />
+        <Route exact path='/SupplyChainProgram' element={<SupplyChainAcademy />} />
+        <Route exact path='/CoachingandMentoring' element={<CoachingandMentoring />} />
+        <Route exact path='/UnlockingAuthenticLeadership' element={<UnlockingAuthenticLeadership />} />
+        <Route exact path='/EntrepreneurialExcellenceSkillsandmindset' element={<Skills />} />
+        <Route exact path='/EmpoweringWomenLeadersForSuccess' element={<EmpoweringWomenLeadersForSuccess />} />
+        <Route exact path='/FosteringFunctionalExpertise' element={<Fostering />} />
+        <Route exact path='/CoachingforProfessionals' element={<Coaching />} />
+        <Route exact path='/ContactUs' element={<ContactUs />} />
+
+
+        {/* <Route exact path='/international-financial-reporting-standards' element={<FinancialReporting />} />
+        <Route exact path='/growth-program-for-accountants' element={<GrowthProgram />} />
+        <Route exact path='/budgeting-variance-analysis-modelling' element={<Budgeting />} />
+        <Route exact path='/financial-modelling-advanced' element={<Modeling />} />
+        <Route exact path='/working-capital-management' element={<WorkingCapital />} />
+        <Route exact path='/finance-for-decision-making-for-non-finance' element={<DecisionMaking />} />
+        <Route exact path='/credit-risk-analysis-modelling' element={<Credit />} /> */}
+
+        {/* Women Pillar */}
+        <Route exact path='/UnloackingMyProfessionalPresence' element={<Unlocking />} />
+        <Route exact path='/HarmonyEmpoweringforbalance' element={<Harmony />} />
+
+        {/* Skills Pillar */}
+        <Route exact path='/StratergyandTransformation' element={<Stratergy />} />
+        <Route exact path='/GlobalLeadershipandCrisisManagement' element={<Global />} />
+        <Route exact path='/LeadingaBusinessinaVUCAWorld' element={<Leading />} />
+
+        {/* Unlocking Pillar */}
+        <Route exact path='/LeadershipandExecutivePresence' element={<Leadership />} />
+        <Route exact path='/EmergingLeaderDevelopment' element={<Emerging />} />
+        <Route exact path='/DevelopmentCenters' element={<Development />} />
+        <Route exact path='/ExecutiveCoaching' element={<Executive />} />
+
+        <Route exact path='/xyz' element={<xyz />} />
+
+        <Route exact path='/isb' element={<NewScreen />} />
+        <Route exact path='/nworx' element={<NewScreen2 />} />
+        <Route exact path='/myso' element={<NewScreen3 />} />
+        <Route exact path='/weace' element={<NewScreen4 />} />
+        <Route exact path='/pdu' element={<NewScreen5 />} />
+
+
+        {/* <Route exact path='/hr-business-partnering-development-program' element={<HRBusiness />} />
+        <Route exact path='/hr-analytics-for-data-driven-decision-making' element={<HRAnalytics />} />
+        <Route exact path='/young-hr-professional-excellence-program' element={<YoungHR />} /> */}
+
+
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+
+
+
---
+++ Frontend/src/Screens/Home.jsx
@@ -0,0 +1,191 @@
+import React, { useEffect, useRef } from 'react'; 
+import { Link } from 'react-router-dom';
+import Navbar from '../Components/Navbar';
+import Carousal from '../Components/Carousal';
+import MissionCard from '../Components/MissionCard';
+import PillarCard from '../Components/PillarCard';
+import Footer from '../Components/Footer';
+import missions from '../Api/Missions';
+import pillars from '../Api/Pillars';
+import { motion } from 'framer-motion';
+import reveal from '../Animations/Variants';
+import { useInView } from 'react-intersection-observer';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Slider from '../Components/Slider';
+import academy from '../Api/Academy';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const Home = () => {
+  const counterRefs = useRef([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const target = entry.target;
+          const countElement = target.querySelector('.count');
+          const speed = 300;
+          const targetVal = parseInt(countElement.dataset.target);
+          let initialVal = 0;
+          const increment = Math.trunc(targetVal / speed);
+
+          const updateCount = () => {
+            if (initialVal < targetVal) {
+              initialVal += increment;
+              countElement.innerText = `${initialVal}+`;
+              setTimeout(updateCount, 10);
+            } else {
+              countElement.innerText = `${targetVal}+`;
+            }
+          };
+          updateCount();
+          observer.unobserve(target);
+        }
+      });
+    });
+
+    counterRefs.current.forEach(ref => {
+      observer.observe(ref);
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  function MissionSection() {
+    const { ref, inView } = useInView({ triggerOnce: true, threshold: 0 });
+
+    return (
+      <motion.div
+        ref={ref}
+        variants={reveal("left", 0.2)}
+        initial="hidden"
+        animate={inView ? "show" : "hidden"}
+        className="mission-section py-5 text-center"
+      >
+        <h2 className="mission-heading">Our Mission</h2>
+        <p className="mx-auto" style={{ maxWidth: '600px', fontSize: "23px" }}>
+          We develop leadership and functional skills across the GCC, empowering individuals and organizations with high-quality training to drive regional growth.
+        </p>
+        <MissionCard data={missions} />
+      </motion.div>
+    );
+  }
+
+  function SliderSection() {
+    const { ref, inView } = useInView({ triggerOnce: true, threshold: 0 });
+
+    return (
+      <motion.div
+        ref={ref}
+        variants={reveal("up", 0.2)}
+        initial="hidden"
+        animate={inView ? "show" : "hidden"}
+        className="mission-section py-5 text-center bg-light"
+      >
+        <h2 className="mission-heading">Functional Training Programs</h2>
+        <Slider data={academy} />
+      </motion.div>
+    );
+  }
+
+  const PillarSection = () => {
+    const { ref } = useInView({ triggerOnce: true, threshold: 0 });
+
+    return (
+      <Container fluid style={{ paddingRight: 0, paddingLeft: 0 }}>
+        <Row noGutters>
+          <Col>
+            <div ref={ref}>
+              <div className='mission-section2 text-center' style={{ width: '100%', overflow: "hidden", padding: "0px", margin: "0px", cursor: 'pointer' }}>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  };
+
+  const PillarSection2 = () => {
+    const { ref } = useInView({ triggerOnce: true, threshold: 0 });
+
+    return (
+      <Container fluid style={{ width: "100%" }}>
+        <Row>
+          <Col>
+            <div className="mission-heading3-container" ref={ref}>
+              <h2 className="mission-heading3">Our Verticals</h2>
+            </div>
+            <PillarCard data={pillars} />
+          </Col>
+        </Row>
+      </Container>
+    );
+  };
+
+  function PartnersSection() {
+    const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.2 });
+  
+    const partners = [
+      { imgSrc: "/images/nvox2.PNG", text: "Enhance leadership skills to meet business results by leveraging AI technology", link: "/nworx" },
+      { imgSrc: "/images/pdeu.PNG", text: "To build intellectual capital and human resources in the fast-developing and competitive energy industry", link: null }, // WRITE /pdu HERE OK
+      { imgSrc: "/images/isblogo.jpg", text: "Transformational learning journeys designed to equip executives across business areas", link: "/isb" },
+      { imgSrc: "/images/download.png", text: "Transforming Coaching & Mentoring by integrating our cutting-edge platform with organizational needs", link: "/weace" },
+      { imgSrc: "/images/MYSOBlue.png", text: "Specialize in combining bespoke content with our micro-courses to meet the specific needs of your organization and employees.", link: "/myso" },
+    ];
+  
+    return (
+      <motion.div
+        ref={ref}
+        initial={{ opacity: 0, x: 50 }}
+        animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: 50 }}
+        transition={{ duration: 0.5 }}
+        className="partners-section py-5 text-center"
+      >
+        <h2 className="mission-heading" style={{ marginBottom: "40px" }}>Our Partners</h2>
+        <Container fluid className="py-5 bg-light" style={{ width: "100%" }}>
+          <Row className="justify-content-center">
+            {partners.map((partner, index) => (
+              <Col key={index} md={4} sm={6} className="mb-4 d-flex justify-content-center">
+                <div className="partner-box text-center" style={{ maxWidth: "450px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                  {partner.link ? (
+                    <Link to={partner.link} style={{ display: "block" }}>
+                      <img src={partner.imgSrc} alt={partner.text} className="img-fluid mb-3" style={{ width: "90%", height: "100px", objectFit: "contain", cursor: "pointer" }} />
+                    </Link>
+                  ) : (
+                    <img src={partner.imgSrc} alt={partner.text} className="img-fluid mb-3" style={{ width: "90%", height: "100px", objectFit: "contain" }} />
+                  )}
+                  <p className="mt-2" style={{ flexGrow: 1 }}>{partner.text}</p>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </motion.div>
+    );
+  }
+
+  function FooterSection() {
+    return (
+      <motion.div variants={reveal("left", 0.2)} initial="hidden" animate="show">
+        <Footer />
+      </motion.div>
+    );
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Carousal />
+      <section id="missions"><MissionSection /></section>
+      <section id="pillars"><PillarSection /></section>
+      <section id="pillars2"><PillarSection2 /></section>
+      <SliderSection />
+      <section id="partners"><PartnersSection /></section>
+      <section id="offices"><FooterSection /></section>
+    </>
+  );
+};
+
+export default Home;
