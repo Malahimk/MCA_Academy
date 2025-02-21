210373fcd1ec71a10866750ea9ba929f9bd2ca74
Malahimk <114082346+Malahimk@users.noreply.github.com>
2025-02-21 09:10:56 +0400

    done

---
+++ Frontend/src/Screens/NewScreen2.jsx
@@ -0,0 +1,183 @@
+import React, { useEffect } from 'react';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
+import finance from '../Api/Finance';
+import { motion } from 'framer-motion';
+import reveal from '../Animations/Variants';
+import { InView } from 'react-intersection-observer';
+import { useInView } from 'react-intersection-observer';
+
+const NewScreen2 = () => {
+
+    useEffect(() => {
+            window.scrollTo(0, 0);
+        }, []);
+
+    const generateURL = (text) => {
+        return text.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)/g, '');
+    };
+
+
+    const boxStyle = {
+        flex: 1,
+        backgroundColor: "#f8f9fa",
+        borderRadius: "10px",
+        boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+        padding: "20px",
+        margin: "10px",
+    };
+
+
+    const scrollToSection = (sectionId) => {
+        console.log("Scrolling to section:", sectionId);
+        const section = document.getElementById(sectionId);
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
+    const handleLinkClick = (event) => {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        window.location.href = href;
+        window.scrollTo(0, 0);
+    };
+
+    const AnimatedColumn = ({ children, delay, direction }) => {
+        const [ref, inView] = useInView({
+            triggerOnce: true,
+            rootMargin: '-100px 0px',
+        });
+
+        return (
+            <motion.div
+                ref={ref}
+                initial="hidden"
+                animate={inView ? "show" : "hidden"}
+                variants={reveal(direction, delay)}
+                transition={{ delay }}
+            >
+                {children}
+            </motion.div>
+        );
+    };
+
+    const slideInFromLeft = {
+        hidden: { opacity: 0, x: -100 },
+        show: { opacity: 1, x: 0, transition: { duration: 0.8 } }
+    };
+
+    const slideInFromRight = {
+        hidden: { opacity: 0, x: 100 },
+        show: { opacity: 1, x: 0, transition: { duration: 0.8 } }
+    };
+
+    const FooterSection = () => {
+        const { ref, inView } = useInView({
+            triggerOnce: true,
+            threshold: 0,
+        });
+
+        return (
+            <motion.div
+                ref={ref}
+                variants={reveal("left", 0.2)}
+                initial="hidden"
+                animate={inView ? "show" : "hidden"}
+            >
+                <Footer />
+            </motion.div>
+        );
+    };
+
+
+    return (
+        <div>
+            <Navbar />
+            <Container>
+                <Row className="mt-5 bg-light py-4 rounded">
+                    <Col xs={12} md={8} lg={8} xl={8}>
+                        <motion.div initial="hidden" animate="show" variants={reveal("right", 0.3)}>
+                            <div className="py-5">
+                                <h2 className="text-left mb-4 mission-heading" style={{ color: "#CE0932", fontWeight: 700 }}>
+                                    NWORX
+                                </h2>
+                                <p style={{ color: "#212529", fontWeight: 600, textAlign: "justify" }} className="text-justify">
+                                This collaboration brings a new focus on transformational learning to prepare future leaders, aligning MCA Academy’s mission to foster business growth with NWORX’s expertise in AI-powered career coaching and placement readiness. NWORX enhances employability by preparing individuals for professional success, improving placement outcomes, and equipping businesses with job-ready talent. Together, we will deliver high-impact training programs that bridge the gap between industry demand and workforce readiness, ensuring professionals excel in their careers.
+                                </p>
+                            </div>
+                        </motion.div>
+                    </Col>
+                    <Col xs={12} md={4} lg={4} xl={4} className="d-flex justify-content-end align-items-center">
+                        <img
+                            src="/images/nvox2.PNG" // Replace with your image URL
+                            alt="ISB Executive Education"
+                            className="img-fluid"
+                            style={{
+                                maxWidth: "400px", // Adjust width as needed
+                                height: "auto",
+                                objectFit: "cover", // Ensure image fits properly within the container
+                            }}
+                        />
+                    </Col>
+                </Row>
+            </Container>
+
+
+            <Container>
+
+                <Row id='international-financial-reporting-standards'>
+
+                    <InView threshold={0.2} triggerOnce>
+                        {({ inView, ref }) => (
+                            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                                <Col xs={12} md={12} className='mt-3' style={{ display: 'flex' }}>
+                                    <motion.div
+                                        ref={ref}
+                                        initial="hidden"
+                                        animate={inView ? "show" : "hidden"}
+                                        variants={slideInFromRight}
+                                        style={{ flex: 1, backgroundColor: "#f8f9fa", borderRadius: '10px', boxShadow: '0 4px 8px rgba(0,0,0,0.1)', padding: '20px', margin: '10px' }}
+                                    >
+                                        <h4 className='mission-heading2'>Key Highlights</h4>
+                                        <ul className='card-list'>
+                                            <li>AI-powered career coaching and mentoring for students and professional</li>
+                                            <li>Enhances workforce performance with personalized, role-based preparation</li>
+                                            <li>Seamless integration with enterprise and educational ecosystems</li>
+                                        </ul>
+                                    </motion.div>
+                                </Col>
+                                {/* <Col xs={12} md={6} className='mt-3' style={{ display: 'flex' }}>
+                                    <motion.div
+                                        ref={ref}
+                                        initial="hidden"
+                                        animate={inView ? "show" : "hidden"}
+                                        variants={slideInFromLeft}
+                                        style={{ flex: 1, backgroundColor: "#f8f9fa", borderRadius: '10px', boxShadow: '0 4px 8px rgba(0,0,0,0.1)', padding: '20px', margin: '10px' }}
+                                    >
+                                        <ul className='card-list'>
+                                            <h4 className='mission-heading2'>FT Executive Education Custom 2024 Rankings</h4> <br />
+                                            <li>#1 in India</li>
+                                            <li>#2 in Asia</li>
+                                            <li>#26 Globally</li>
+                                        </ul>
+                                    </motion.div>
+                                </Col> */}
+                            </div>
+                        )}
+                    </InView>
+                </Row>
+
+            </Container>
+
+            <br /> <br />
+            <FooterSection />
+        </div >
+    );
+};
+
+export default NewScreen2;
